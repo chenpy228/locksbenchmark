7e7e55a9fb699e047f5473140ac4cccc516e1aed
chenpy228 <chpei@foxmail.com>
2020-02-29 20:30:37 +0800

    add new synchronization mechanism

--- src/main/java/xyz/supercoder/locksbenchmark/Counter.java
+++ src/main/java/xyz/supercoder/locksbenchmark/Counter.java
@@ -2,6 +2,10 @@ package xyz.supercoder.locksbenchmark;
 
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.LongAdder;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.StampedLock;
 
 public enum Counter {
 
@@ -59,6 +63,113 @@ public enum Counter {
         @Override
         public void increment() {
             this.value.increment();
+        }
+    },
+
+    StampedLock() {
+        private final StampedLock stampedLock = new StampedLock();
+        private long value = 0;
+
+        @Override
+        public void reset() {
+            this.value = 0;
+        }
+
+        @Override
+        public long get() {
+            long stamp = stampedLock.readLock();
+            try {
+                return this.value;
+            } finally {
+                stampedLock.unlock(stamp);
+            }
+        }
+
+        @Override
+        public void increment() {
+            long stamp = stampedLock.writeLock();
+            try {
+                this.value++;
+            } finally {
+                stampedLock.unlock(stamp);
+            }
+        }
+    },
+
+    RWLock() {
+        private final ReadWriteLock rwlock = new ReentrantReadWriteLock();
+        private final Lock rlock = rwlock.readLock();
+        private final Lock wlock = rwlock.writeLock();
+
+        private long value = 0;
+
+        @Override
+        public void reset() {
+            this.value = 0;
+        }
+
+        @Override
+        public long get() {
+            rlock.lock();
+            try {
+                return this.value;
+            } finally {
+                rlock.unlock();
+            }
+        }
+
+        @Override
+        public void increment() {
+            wlock.lock();
+            try {
+                this.value++;
+            } finally {
+                wlock.unlock();
+            }
+        }
+    },
+
+    Synchronized() {
+        private final Object lock = new Object();
+
+        private long value = 0;
+
+        @Override
+        public void reset() {
+            this.value = 0;
+        }
+
+        @Override
+        public long get() {
+            synchronized (lock) {
+                return this.value;
+            }
+        }
+
+        @Override
+        public void increment() {
+            synchronized (lock) {
+                this.value++;
+            }
+        }
+    },
+
+    Volatile() {
+        private volatile long value = 0;
+
+        @Override
+        public void reset() {
+            this.value = 0;
+        }
+
+        @Override
+        public long get() {
+            return this.value;
+        }
+
+        @Override
+        public void increment() {
+            this.value++;
         }
     };
 
