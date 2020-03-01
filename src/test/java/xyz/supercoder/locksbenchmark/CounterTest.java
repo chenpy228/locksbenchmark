786b4df4719277c69671184c2614ca72cf20f845
chenpy228 <chpei@foxmail.com>
2020-03-01 11:45:59 +0800

    add unit test cases

---
+++ src/test/java/xyz/supercoder/locksbenchmark/CounterTest.java
@@ -0,0 +1,23 @@
+package xyz.supercoder.locksbenchmark;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CounterTest {
+    @Test
+    public void testReset() {
+        for (Counter counter : Counter.values()) {
+            counter.reset();
+            Assert.assertEquals(0, counter.get());
+        }
+    }
+
+    @Test
+    public void testIncrement() {
+        for (Counter counter : Counter.values()) {
+            counter.reset();
+            counter.increment();
+            Assert.assertEquals(1, counter.get());
+        }
+    }
+}
